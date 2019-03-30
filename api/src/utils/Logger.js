4c700701c4671ffb5c0299ca1e3186455cafbb5b
andrei <andrei@macrobyte.ro>
2019-03-31 00:32:10 +0200

    extract logs from docker container

--- api/src/utils/Logger.js
+++ api/src/utils/Logger.js
@@ -3,7 +3,7 @@ const { createLogger, format, transports } = require('winston');
 const path = require('path');
 const appInfo = require('./AppInfo');
 
-const logDir = '../data/log/App';
+const logDir = process.env.LOG_DIR;
 const filename = path.join(logDir, `${appInfo.appName}_.log`);
 
 
