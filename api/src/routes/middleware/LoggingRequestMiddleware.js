b0c6c7f80ae700fed438fa5c82f83b5f28a61181
andrei <andrei@macrobyte.ro>
2019-03-31 01:10:51 +0200

    fix log to try and get client's real ip

--- api/src/routes/middleware/LoggingRequestMiddleware.js
+++ api/src/routes/middleware/LoggingRequestMiddleware.js
@@ -4,7 +4,7 @@ const logger = require('../../utils/Logger');
 const appInfo = require('../../utils/AppInfo');
 
 module.exports = function middlewareLogger(req, res, next) {
-	const originIp = req.headers['x-real-ip'] || req.connection.remoteAddress;
+	const originIp = req.ip || req.headers['x-real-ip'] || req.connection.remoteAddress;
 	logger.debug(`[${appInfo.appName}][${appInfo.appVersion}][request] #[${originIp}]# [${req.method}] - [${req.path}] - [${JSON.stringify(req.body, null, 2)}]`);
 	res.on('finish', () => {
 		logger.debug(`[${appInfo.appName}][${appInfo.appVersion}][response] #[${originIp}]# [${req.method}] - [${res.statusCode}] - [${req.path}]`);
