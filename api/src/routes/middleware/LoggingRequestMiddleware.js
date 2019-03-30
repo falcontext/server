d22bfabd41687a33ad73508694e4b8f46418c435
andrei <andrei@macrobyte.ro>
2019-03-31 01:31:50 +0200

    fix logger to log the correct client ip

--- api/src/routes/middleware/LoggingRequestMiddleware.js
+++ api/src/routes/middleware/LoggingRequestMiddleware.js
@@ -4,12 +4,10 @@ const logger = require('../../utils/Logger');
 const appInfo = require('../../utils/AppInfo');
 
 module.exports = function middlewareLogger(req, res, next) {
-	logger.debug(req.ip);
-	logger.debug(req.originIp);
-	logger.debug(req.headers['x-real-ip']);
-	logger.debug(req.connection.remoteAddress);
-	logger.debug(req.headers['x-forwarded-for']);
-	const originIp = req.ip || req.headers['x-real-ip'] || req.connection.remoteAddress;
+	
+	const originIp = (req.headers['x-forwarded-for'] || '').split(',').pop() 
+					|| req.headers['x-real-ip'] 
+					|| req.connection.remoteAddress
 	logger.debug(`[${appInfo.appName}][${appInfo.appVersion}][request] #[${originIp}]# [${req.method}] - [${req.path}] - [${JSON.stringify(req.body, null, 2)}]`);
 	res.on('finish', () => {
 		logger.debug(`[${appInfo.appName}][${appInfo.appVersion}][response] #[${originIp}]# [${req.method}] - [${res.statusCode}] - [${req.path}]`);
